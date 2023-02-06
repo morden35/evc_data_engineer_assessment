0f6d8bd26e6509e2a27a92a10f85b1305d4a7f5b
Michelle Orden <82831800+morden35@users.noreply.github.com>
2023-02-05 19:37:15 -0600

    changed union all to union

--- 02_sql_queries/all_records_generation_TO_EDIT.sql
+++ 02_sql_queries/all_records_generation_TO_EDIT.sql
@@ -21,7 +21,7 @@ create table s_f33890ec9c814bb1d9227550d09e9947.all_records as (
         , county
         , gender
         , date_of_birth
-        , phone_number
+        -- , phone_number
         , email_address
         , updated_at
         , party
@@ -43,50 +43,7 @@ create table s_f33890ec9c814bb1d9227550d09e9947.all_records as (
         , evc_year
     from public.qc_vendor_data
 )
-union all
-select
-        vendor_id as application_id
-        , org_id
-        , shift_id
-        , first_name
-        , middle_name
-        , last_name
-        , name_suffix
-        , voting_street_address_one
-        , voting_street_address_two
-        , voting_city
-        , voting_state
-        , voting_zipcode
-        , mailing_street_address_one
-        , mailing_street_address_two
-        , mailing_city
-        , mailing_state
-        , mailing_zipcode
-        , county
-        , gender
-        , date_of_birth
-        , phone_number
-        , email_address
-        , updated_at
-        , party
-        , name_prefix
-        , ethnicity
-        , latitude
-        , longitude
-        , completed
-        , registration_date
-        , shift_type
-        , locations_state
-        , program_type
-        , program_sub_type
-        , collection_medium
-        , office
-        , field_start
-        , field_end
-        , evc_month
-        , evc_year
-from public.vendor_x_registrations_raw
-union all
+union
 select
   vendor_id as application_id,
   org as org_id,
@@ -95,7 +52,7 @@ select
   middle_name,
   last_name,
   name_suffix,
-  home_address as voting_street_address_one,
+  home_address as voting_street_address_on,
   home_unit as voting_street_address_two,
   home_city as voting_city,
   home_state as voting_state,
@@ -108,15 +65,15 @@ select
   mailing_county as county,
   predicted_gender as gender,
   date_of_birth,
-  phone as phone_number,
+  -- CAST(phone as int) as phone_number
   email_address,
-  -- NULL as updated_at,
+  CAST(NULL AS timestamp) as updated_at,
   party,
   salutation as name_prefix,
-  -- NULL as ethnicity,
-  -- NULL as latitude,
-  -- NULL as longitude,
-  -- NULL as completed,
+  CAST(NULL AS varchar) as ethnicity,
+  CAST(NULL AS float8) as latitude,
+  CAST(NULL AS float8) as longitude,
+  CAST(NULL AS bool) as completed,
   registration_date,
   shift_type,
   program_state as locations_state,
@@ -125,8 +82,8 @@ select
   registration_source as collection_medium,
   office,
   field_start,
-  field_end
-  -- NULL as evc_month,
-  -- NULL as evc_year
+  field_end,
+  CAST(NULL AS varchar) as evc_month,
+  CAST(NULL AS varchar) as evc_year
 from
-  vendor_x_registrations_raw;
+  vendor_x_registrations_raw
